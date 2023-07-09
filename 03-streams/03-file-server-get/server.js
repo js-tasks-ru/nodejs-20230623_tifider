a043043fa4336bfaef548d6c0be790d9800f4446
romanselin <Roman.Selin@ringcentral.com>
2023-07-09 16:02:51 +0400

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,10 +10,22 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'GET':
+      if (filepath.includes('/', filepath.indexOf('files/') + 6)) {
+        res.statusCode = 400;
+        res.end('Nested directories are not supported.');
+      }
 
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+
+      stream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('No such file.');
+        }
+      });
       break;
 
     default:

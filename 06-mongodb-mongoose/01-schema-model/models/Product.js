a75ae45ffe84ab8449cfdcc37d56b0b16ee818d7
romanselin <Roman.Selin@ringcentral.com>
2023-07-21 01:26:57 +0400

    add solution for 6-module 1-task

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: mongoose.ObjectId,
+        required: true,
+    },
+    subcategory: {
+        type: mongoose.ObjectId,
+        required: true,
+    },
+    images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);

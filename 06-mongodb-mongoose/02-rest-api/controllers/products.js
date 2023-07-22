068b8809ed48e907b4b8cd95955485f0c3d583e8
romanselin <Roman.Selin@ringcentral.com>
2023-07-23 00:28:59 +0400

    add solution for 6-module 2-task

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,38 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+const ObjectId = require('mongoose').Types.ObjectId;
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const products = await Product.find({subcategory: ObjectId(subcategory)});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+
+  ctx.body = {products: products.map(mapProduct)};
+
+  await next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  let {id} = ctx.params;
+  let product;
 
+  try {
+    product = await Product.findById(id);
+  } catch (error) {
+    if (error.kind === 'ObjectId') {
+      ctx.throw(400);
+    }
+  }
+
+  if (!product) ctx.throw(404);
+
+  ctx.body = {product: mapProduct(product)};
+};

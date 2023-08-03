700d3e5ff5e699fd8725e5312c719ec33bc41673
romanselin <Roman.Selin@ringcentral.com>
2023-08-03 10:58:08 +0300

    add solution for 7-module 2-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,21 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, 'Не указан email');
+
+  const user = await User.findOne({email});
+
+  if (user) return done(null, user);
+
+  if (!user) {
+    try {
+      const user = await User.create({email, displayName});
+
+      return done(null, user);
+    } catch (error) {
+      return done(error, null, 'Некорректный email.');
+    }
+  }
+
   done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
 };

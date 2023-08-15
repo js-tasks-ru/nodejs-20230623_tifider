1a97b967935a35891fff91d3419ee6f1163b5e9e
romanselin <Roman.Selin@ringcentral.com>
2023-08-15 18:11:47 +0300

    add solution for 8-module 2-task

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,22 @@
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const {phone, product, address} = ctx.request.body;
+    const user = ctx.user;
+
+    const order = await Order.create({user, phone, product, address});
+
+    await sendMail({template: 'order-confirmation', locals: {product}, to: user.email, subject: 'Заказ'});
+
+    ctx.body = {order: order.id};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    const orders = await Order.find({user: ctx.user});
+
+    ctx.body = {
+        orders: orders.map(mapOrder)
+    };
 };
